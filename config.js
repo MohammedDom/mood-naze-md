8d5f6286e111da34972417de08a0f2f89de9c4ca
MohammedDom <133832109+MohammedDom@users.noreply.github.com>
2023-06-25 07:25:08 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,70 @@
+/**
+   * Create By Dika Ardnt.
+   * Recode By Naze Dev
+   * Contact Me on wa.me/967778447474
+   * Follow https://github.com/nazedev
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'e9c9bf8c4fdcd6c56114c2db69032149-5e8ca13f-dcd7-410d-809c-8b6cbe8c5160',
+}
+
+// Other
+global.owner = ['967730061977']
+global.ownernomer = "967730061977"
+global.premium = ['967730061977']
+global.packname = 'Sticker By'
+global.author = 'Naze Dev'
+global.sessionName = 'nazedev'  //jangan diganti bro nanti error
+global.jumlha = '999'
+global.jumhal = '100000000000000'
+global.jumlah = '1000000000'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+
+// Setting Mess
+global.mess = {
+    success: '✅Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa / Chat Owner',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    error: 'Error!',
+    errapi: 'Error Mungkin Apikey Tidak Valid!',
+    errmor: 'Error Kesalahan Sistem',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+
+// Limit
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
+}
+
+
+// Engak Ngaruh, Belum Kepasang
+global.botname = 'DOM BOT'
+global.akulaku = 'Bot By DOM'
+global.ytname = 'YT NAZE' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
